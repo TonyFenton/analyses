65e9e8a01ab86c4e36a7aba0312d4cfe4c6ca1da
Tony <tony.fenton3@gmail.com>
2017-07-08 15:00:35 +0200

    Improve matrix JavaScript

--- web/js/matrix/index.js
+++ web/js/matrix/index.js
@@ -1,4 +1,23 @@
 $(function () {
+
     var matrix = new Matrix();
-    // each button addItem ale naprzykład tylko kiedy nowa lista
+
+    matrix.addButtons.on('click', function () {
+        matrix.addItem($(this));
+    });
+    matrix.removeButtons.on('click', function () {
+        matrix.removeItem($(this));
+    });
+    matrix.form.on('submit', function () {
+        matrix.updateItemsNames();
+
+        // updateItemsNames test
+        // $('.matrix-item input').each(function () {
+        //     console.log($(this).attr('name'));
+        //
+        // });
+        //
+        // return false;
+    });
+
 });
