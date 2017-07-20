3042e14e534106ffba41b9a5b9f0ac4ae6752ff1
Tony <tony.fenton3@gmail.com>
2017-07-20 23:29:39 +0200

    Fix translation for HTML5 required message.

--- web/js/base.js
+++ web/js/base.js
@@ -1,6 +1,10 @@
 var translator = new Translator();
 
 /* Replace invalid message on element with attr required="required" */
-$('[required="required"]').on('invalid', function () {
-    this.setCustomValidity(translator.trans('required_field'));
+$('input[required="required"]').on('invalid', function () {
+    if ($(this).val() === '') {
+        this.setCustomValidity(translator.trans('required_field'));
+    } else {
+        this.setCustomValidity('');
+    }
 });
