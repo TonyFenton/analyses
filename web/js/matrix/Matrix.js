4e64859789a052c001149a7fdfcbf5c5a043f6a8
Tony <tony.fenton3@gmail.com>
2017-07-26 00:00:06 +0200

    Create preview mode

--- web/js/matrix/Matrix.js
+++ web/js/matrix/Matrix.js
@@ -28,8 +28,48 @@ function Matrix() {
         });
     };
 
+    this.turnOnPreviewMode = function () {
+        this.matrix.find('.alert').remove();
+        $('.remove-button').removeClass('icon-cancel').addClass('icon-dot');
+        this.addButtons.hide();
+        this.matrix.find("input").css({
+            'background-color': 'transparent',
+            'border': 0,
+            'box-shadow': 'none',
+            'padding-left': 0,
+            'padding-right': 0
+        }).prop("readonly", true);
+        this.matrix.find('button').prop('disabled', true);
+    };
+
+    this.turnOffPreviewMode = function () {
+        $('.remove-button').removeClass('icon-dot').addClass('icon-cancel');
+        this.addButtons.show();
+        this.matrix.find("input").css({
+            'background-color': '',
+            'border': '',
+            'box-shadow': '',
+            'padding-left': '',
+            'padding-right': ''
+        }).prop("readonly", false);
+        this.matrix.find('button').prop('disabled', false);
+    };
+
     this.form = $('#matrix-form');
     this.matrix = $('#matrix');
     this.addButtons = this.matrix.find('.add-button');
     this.removeButtons = this.matrix.find('.remove-button');
+
+    /* Constructor */
+
+    var thisObj = this;
+
+    var viewModeCheckbox = $('#preview-mode');
+    viewModeCheckbox.on('change', function () {
+        if (this.checked) {
+            thisObj.turnOnPreviewMode();
+        } else {
+            thisObj.turnOffPreviewMode();
+        }
+    });
 }
