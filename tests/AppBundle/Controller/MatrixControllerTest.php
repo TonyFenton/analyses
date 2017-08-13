afbb29b9ee7db3b3df0c1316a68d51544abae4bc
Tony <tony.fenton3@gmail.com>
2017-08-13 17:58:26 +0200

    Fix tests after changed the class name

--- tests/AppBundle/Controller/MatrixControllerTest.php
+++ tests/AppBundle/Controller/MatrixControllerTest.php
@@ -12,7 +12,7 @@ class MatrixControllerTest extends FunctionalTestHelper
         $this->requestGet('/swot-analysis');
         $this->checkElementsQty(4, 'li.prototype-item');
         $this->checkElementsQty(9, '.matrix-cell');
-        $this->checkElementsQty(8, '.head-list');
+        $this->checkElementsQty(8, '.matrix-head-list');
         $this->assertSame('Strengths', $this->crawler->filter('#swot_b2field')->attr('placeholder'));
     }
 
